6252e141b1479e1fc79a14f410f0142330d9de2a
mahmoud-abo-al-ela <mahmoud.aboalela2786@gmail.com>
2025-12-28 19:09:15 +0200

    refactor(home): remove unused CTASection export
    - Remove CTASection export from home component index
    - Simplify component exports by eliminating unused module
    - Clean up barrel export file to only include active components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,4 +2,3 @@ export { HeroSection } from "./HeroSection";
 export { StatsSection } from "./StatsSection";
 export { EventsSection } from "./EventsSection";
 export { FeaturesSection } from "./FeaturesSection";
-export { CTASection } from "./CTASection";
